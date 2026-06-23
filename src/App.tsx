0296fb907e68b05136b5297b9441c5a9dd238390
Vishakha Rastogi <vishakha9814@gmail.com>
2026-06-23 21:48:01 +0530

    Board layout setup

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,16 @@ import InputPanel from "./components/InputPanel";
 import OutputPanel from "./components/OutputPanel";
 
 function App() {
+  const boardDimensions = {
+    x: 5,
+    y: 5,
+  };
+
   return (
     <>
       Robot Game
       <InputPanel />
-      <Board />
+      <Board x={boardDimensions.x} y={boardDimensions.y} />
       <OutputPanel />
     </>
   );
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,7 +1,52 @@
-function Board() {
+type BoardProps = {
+  x: number;
+  y: number;
+};
+
+function Board(props: BoardProps) {
+  const prepareBoard = (x: number, y: number) => {
+    let board = [];
+    for (let i = x - 1; i >= 0; i--) {
+      let row = [];
+      for (let j = 0; j < y; j++) {
+        row.push([j, i]);
+      }
+      board.push(row);
+    }
+
+    return (
+      <>
+        {board.map((row) => {
+          return (
+            <div key={row.toString()} style={{ width: "fit-content" }}>
+              <>
+                {row.map((cell) => {
+                  return (
+                    <span key={cell.toString()} style={{ padding: "5px" }}>
+                      #{cell}
+                    </span>
+                  );
+                })}
+              </>
+            </div>
+          );
+        })}
+      </>
+    );
+  };
+
   return (
     <>
-      <section>BOARD</section>
+      <section
+        style={{
+          border: "1px solid",
+          margin: "auto",
+          width: "fit-content",
+          padding: "10px",
+        }}
+      >
+        {prepareBoard(props.x, props.y)}
+      </section>
     </>
   );
 }
